6197f3f1ab0705e8db13f86f67def8032c436f22
pfcm <pfcmathews@gmail.com>
2017-12-01 23:43:51 +1300

    day 1 part b

--- days/First.hs
+++ days/First.hs
@@ -1,4 +1,5 @@
 import Data.Char
+import Data.Maybe
 
 
 circularSqueeze :: Eq a => [a] -> [a]
@@ -15,10 +16,29 @@ circularSqueeze list =
     in squeeze list
 
 
+cyclicShift :: Int -> [a] -> [a]
+cyclicShift _ [] = []
+cyclicShift k xs = zipWith const (drop k . cycle $ xs) xs
+
+
+maybeEqual :: Eq a => a -> a -> Maybe a
+maybeEqual x y = if x == y then Just x else Nothing
+
+
+halfwaySqueeze :: Eq a => [a] -> [a]
+halfwaySqueeze [] = []
+halfwaySqueeze xs =
+  catMaybes . zipWith maybeEqual xs $ cyclicShift (length xs `div` 2) xs
+
+
 solveCaptcha :: [Int] -> Int
 solveCaptcha = sum . circularSqueeze
 
 
+solveCaptcha2 :: [Int] -> Int
+solveCaptcha2 = sum . halfwaySqueeze
+
+
 main = do
   inputs <- getLine
-  print . solveCaptcha . map digitToInt $ inputs
+  print . solveCaptcha2 . map digitToInt $ inputs
