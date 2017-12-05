b3c2dedf78c5bdb04442db6a5a4b578ecb672cc5
pfcm <pfcmathews@gmail.com>
2017-12-05 22:36:08 +1300

    less stupid version, still slow

--- days/Fifth.hs
+++ days/Fifth.hs
@@ -1,11 +1,16 @@
+import Control.Monad.ST
+import Data.STRef
 import qualified Data.Text as T
 import qualified Data.Text.IO as T.IO
 import Data.Text.Read (decimal, signed)
-import qualified Data.Vector as V
-import Data.Vector ((!?), (//))
+import qualified Data.Vector.Unboxed as V
+import Data.Vector.Unboxed ((!?), (//))
 
 countSteps :: [Int] -> Int
 countSteps = length . steps2 . V.fromList
+
+countSteps' :: [Int] -> Int
+countSteps' = steps2' . V.fromList
 
 -- part 1
 -- this doesn't feel real great
@@ -34,7 +39,31 @@ steps2 jumpTable = go jumpTable [0]
                     else 1
         Nothing -> xs
 
+-- see if we can not generate the whole list
+steps2' :: V.Vector Int -> Int
+steps2' jumpTable = go jumpTable 0 0
+  where
+    go jtab pos steps =
+      case jtab !? pos of
+        Just jump -> go (jtab // [(pos, jump + inc)]) (pos + jump) (steps + 1)
+          where inc =
+                  if jump >= 3
+                    then -1
+                    else 1
+        Nothing -> steps
+
+-- try the ST monad so we don't have to keep everything
+-- stepsST :: V.Vector Int -> Int
+-- stepsST jumpTable = runST $ do
+--   p <- newSTRef 0
+--   steps <- newSTRef 0
+--   jt <- V.thaw jumpTable
+--   run jt p
+--   where
+--     run jtab pos =
+--       case jtab !? pos of
+--         Just jump ->
 parse :: T.Text -> Either String [Int]
 parse = fmap (map fst) . mapM (signed decimal) . T.lines
 
-main = T.IO.interact (T.pack . show . fmap countSteps . parse)
+main = T.IO.interact (T.pack . show . fmap countSteps' . parse)
