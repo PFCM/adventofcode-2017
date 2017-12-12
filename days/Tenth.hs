1787514afbcd5beed2da91ea2ab855bea0d1a810
pfcm <pfcmathews@gmail.com>
2017-12-12 14:03:02 +1300

    refactor to make part 2 possible

--- days/Tenth.hs
+++ days/Tenth.hs
@@ -1,15 +1,33 @@
+{-# LANGUAGE RecordWildCards #-}
+
+import Data.Char (ord)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T.IO
 import Data.Text.Read (decimal)
 import qualified Data.Vector.Unboxed as V
 import Data.Vector.Unboxed ((!), (//))
 
-part1 :: V.Vector Int -> Int
-part1 = V.product . V.slice 0 2
+data Knots = Knots
+  { tMarks :: V.Vector Int
+  , tSkip :: Int
+  , tPos :: Int
+  }
 
-parse :: T.Text -> Either String [Int]
-parse = mapM (fmap fst . decimal) . T.split (== ',')
+initialKnots :: Int -> Knots
+initialKnots size = Knots (V.generate size id) 0 0
 
+-- part 1 is not so bad
+parse1 :: T.Text -> Either String [Int]
+parse1 = mapM (fmap fst . decimal) . T.split (== ',')
+
+part1 :: [Int] -> Int
+part1 = V.product . V.slice 0 2 . tMarks . runSwaps (initialKnots 256)
+
+-- part 2 just adds a bunch of weird stuff
+parse2 :: T.Text -> Either String [Int]
+parse2 = Right . map ord . T.unpack
+
+-- down here is the shared stuff
 flipSlice :: V.Unbox a => Int -> Int -> V.Vector a -> V.Vector a
 flipSlice start len vec = vec // update
   where
@@ -18,13 +36,16 @@ flipSlice start len vec = vec // update
     indices = take len . drop start . cycle $ [0 .. size - 1]
     values = reverse . map (vec !) $ indices
 
-runSwaps :: Int -> [Int] -> V.Vector Int
-runSwaps size steps = go (V.generate size id) steps 0 0
+runSwaps :: Knots -> [Int] -> Knots
+runSwaps k [] = k
+runSwaps Knots {..} (l:ls) = runSwaps newK ls
   where
-    go vec [] _ _ = vec
-    go vec (l:ls) skip pos =
-      go (flipSlice pos l vec) ls (skip + 1) ((pos + l + skip) `mod` size)
+    newK =
+      Knots
+        (flipSlice tPos l tMarks)
+        (tSkip + 1)
+        ((tPos + l + tSkip) `mod` V.length tMarks)
 
 main = do
   inputs <- T.IO.getContents
-  print (part1 . runSwaps 256 <$> parse inputs)
+  print (part1 <$> parse1 inputs)
