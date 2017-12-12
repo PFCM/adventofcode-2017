be65dba4e45d76010b72d9c26cc3d40b37265bcf
pfcm <pfcmathews@gmail.com>
2017-12-12 15:47:45 +1300

    ~broken~ apparently working part 2

--- days/Tenth.hs
+++ days/Tenth.hs
@@ -1,11 +1,13 @@
 {-# LANGUAGE RecordWildCards #-}
 
+import Data.Bits (xor)
 import Data.Char (ord)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T.IO
 import Data.Text.Read (decimal)
 import qualified Data.Vector.Unboxed as V
 import Data.Vector.Unboxed ((!), (//))
+import Numeric (showHex)
 
 data Knots = Knots
   { tMarks :: V.Vector Int
@@ -25,7 +27,25 @@ part1 = V.product . V.slice 0 2 . tMarks . runSwaps (initialKnots 256)
 
 -- part 2 just adds a bunch of weird stuff
 parse2 :: T.Text -> Either String [Int]
-parse2 = Right . map ord . T.unpack
+parse2 = Right . map ord . T.unpack . T.stripEnd
+
+part2 :: [Int] -> String
+part2 steps =
+  let steps' = steps ++ [17, 31, 73, 47, 23]
+      runMany 0 knots = knots
+      runMany i knots = runMany (i - 1) . flip runSwaps steps' $ knots
+      sparse = runMany 64 (initialKnots 256)
+      dense = map (V.foldr1' xor) . vectorChunks 16 . tMarks $ sparse
+      pad [c] = ['0', c]
+      pad cs = cs
+  in concatMap (pad . (`showHex` "")) dense
+
+vectorChunks :: V.Unbox a => Int -> V.Vector a -> [V.Vector a]
+vectorChunks = go 0
+  where
+    go pos size vec
+      | pos < V.length vec = V.slice pos size vec : go (pos + size) size vec
+      | otherwise = []
 
 -- down here is the shared stuff
 flipSlice :: V.Unbox a => Int -> Int -> V.Vector a -> V.Vector a
@@ -48,4 +68,7 @@ runSwaps Knots {..} (l:ls) = runSwaps newK ls
 
 main = do
   inputs <- T.IO.getContents
+  putStrLn "Part 1:"
   print (part1 <$> parse1 inputs)
+  putStrLn "Part 2:"
+  print (part2 <$> parse2 inputs)
