caaf8e77c15ee055b880a90a6efd4c0d31150596
pfcm <pfcmathews@gmail.com>
2017-12-12 20:52:33 +1300

    day 12 part 1, nice simple graph algol to start

---
+++ days/Twelfth.hs
@@ -0,0 +1,51 @@
+import qualified Data.Map as M
+import Data.Map ((!))
+import qualified Data.Set as S
+import Text.Parsec
+import Text.Parsec.String (Parser)
+
+type Graph = M.Map Node (S.Set Node)
+
+type Node = Int
+
+-- do a depth first search, returning the nodes visited
+dfs :: Node -> Graph -> [Node]
+dfs start graph = go [start] []
+  where
+    go [] path = path
+    go (n:ns) path
+      | n `elem` path = go ns path -- slow :(
+      | otherwise = go (neighbours n ++ ns) (n : path)
+    neighbours n = S.toList $ graph ! n
+
+-- turn our list-of-lists graph representation into something a bit more
+-- efficient for searching in
+fromLists :: [[Int]] -> Graph
+fromLists adjacencies = M.fromList (zip [0 ..] neighbourhoods)
+  where
+    neighbourhoods = map S.fromList adjacencies
+
+lineParser :: Parser [Int]
+lineParser = do
+  skipMany1 digit
+  space
+  string "<->"
+  space
+  nums <- sepBy (many1 digit) (string ", ")
+  return (map read nums)
+
+-- assumes ascending order, seems to hold for the provided input
+parser :: Parser [[Int]]
+parser = sepEndBy1 lineParser newline
+
+withStdInput :: Parser a -> IO a
+withStdInput p = do
+  result <- parse (p <* eof) "<input>" <$> getContents
+  either (error . show) return result
+
+main =
+  withStdInput parser >>= \parsed -> do
+    putStrLn "Part 1"
+    let graph = fromLists parsed
+    print . length . dfs 0 $ graph
+    putStrLn "Part 2"
