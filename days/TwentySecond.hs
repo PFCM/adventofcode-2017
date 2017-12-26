a354068a14ebf606411eee9a2ded737eaddf2c8b
pfcm <pfcmathews@gmail.com>
2017-12-26 18:54:08 +1300

    successful 22, although suspiciously seems to fail on the provided examples

--- days/TwentySecond.hs
+++ days/TwentySecond.hs
@@ -1,8 +1,7 @@
 {-# LANGUAGE RecordWildCards #-}
 
 import Data.List (foldl')
-import qualified Data.Set as S
-import Debug.Trace
+import qualified Data.Map.Strict as M
 
 type Position = (Int, Int)
 
@@ -11,12 +10,19 @@ data Direction
   | E
   | S
   | W
+  deriving (Show)
+
+data Node
+  = Weakened
+  | Infected
+  | Flagged
+  deriving (Show)
 
 data Infector = Infector
   { pos :: Position
   , dir :: Direction
   , count :: Int
-  }
+  } deriving (Show)
 
 right :: Direction -> Direction
 right N = E
@@ -27,12 +33,20 @@ right W = N
 left :: Direction -> Direction
 left = right . right . right
 
-act :: S.Set Position -> Infector -> (S.Set Position, Infector)
+act :: M.Map Position Node -> Infector -> (M.Map Position Node, Infector)
 act infd Infector {..} =
-  if S.member pos infd
-    then (S.delete pos infd, Infector (step (right dir) pos) (right dir) count)
-    else ( S.insert pos infd
-         , Infector (step (left dir) pos) (left dir) (count + 1))
+  case M.lookup pos infd of
+    Just Flagged ->
+      ( M.delete pos infd
+      , Infector (step (right . right $ dir) pos) (right . right $ dir) count)
+    Just Infected ->
+      ( M.insert pos Flagged infd
+      , Infector (step (right dir) pos) (right dir) count)
+    Just Weakened ->
+      (M.insert pos Infected infd, Infector (step dir pos) dir (count + 1))
+    Nothing ->
+      ( M.insert pos Weakened infd
+      , Infector (step (left dir) pos) (left dir) count)
 
 step :: Direction -> Position -> Position
 step N (x, y) = (x, y - 1)
@@ -40,15 +54,16 @@ step E (x, y) = (x + 1, y)
 step S (x, y) = (x, y + 1)
 step W (x, y) = (x - 1, y)
 
-parse :: String -> S.Set Position
+parse :: String -> M.Map Position Node
 parse inputs =
-  S.fromList . concatMap parseLine . zip [-size ..] . lines $ inputs
+  M.fromList . concatMap parseLine . zip [-size ..] . lines $ inputs
   where
     size = (length . lines $ inputs) `div` 2
 
-parseLine :: (Int, String) -> [Position]
+parseLine :: (Int, String) -> [(Position, Node)]
 parseLine (y, line) =
-  map (\(x, _) -> (x, y)) . filter ((== '#') . snd) . zip [-size ..] $ line
+  map (\(x, _) -> ((x, y), Infected)) . filter ((== '#') . snd) . zip [-size ..] $
+  line
   where
     size = length line `div` 2
 
@@ -58,4 +73,4 @@ main = do
       infector = Infector (0, 0) N 0
       results = iterate (uncurry act) (initialInfection, infector)
   putStrLn "Part 1"
-  print $ count . snd $ results !! 10000
+  print $ count . snd $ results !! 10000000
